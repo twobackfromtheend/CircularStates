556f931ee30abb12e403093f8407d6245a7fd4b3
Harry Xie <harry1996@gmail.com>
2020-12-24 20:13:00 +0000

    add rb-87

--- system/hamiltonians/hamiltonians.py
+++ system/hamiltonians/hamiltonians.py
@@ -170,7 +170,8 @@ if __name__ == '__main__':
 
     n = 56
     # stark_map = arc.StarkMap(arc.Hydrogen())
-    stark_map = arc.StarkMap(arc.Rubidium())
+    # stark_map = arc.StarkMap(arc.Rubidium())
+    stark_map = arc.StarkMap(arc.Rubidium87())
 
     states, matrices = generate_matrices(n, stark_map)
 
