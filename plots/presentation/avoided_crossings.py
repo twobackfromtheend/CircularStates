e2b4fb02762fc6e7513306fc7956a395f75f3860
Harry Xie <harry1996@gmail.com>
2021-06-09 13:31:18 +0100

    removed old sr code, fixed wrong avoided_crossings.py file

--- plots/presentation/avoided_crossings.py
+++ plots/presentation/avoided_crossings.py
@@ -10,63 +10,45 @@ from scipy.constants import e as C_e, h as C_h, hbar as C_hbar, physical_constan
 
 from plots.presentation.utils import setup_plot, save_current_fig
 from system.hamiltonians.hamiltonians import load_hamiltonian
-from system.hamiltonians.utils import plot_matrices
+from system.hamiltonians.utils import plot_matrices, diagonalise_by_ml, diagonalise_for_n1n2
 from system.states import States, Basis
 from system.transformations.utils import load_transformation, transform_basis
 from timer import timer
 
-n = 56
+plot_fig17a = False
+plot_fig17b = True
+
+
+n = 51
 
 with timer("Generating states"):
-    states_n_l_ml_ms = States(n, basis=Basis.N_L_ML_MS).states
-    states = States(n, basis=Basis.N1_N2_ML_MS).states
+    states = States(n, basis=Basis.N_L_ML_MS_RELEVANT)
+    # states = States(n, basis=Basis.N_L_ML_MS)
 
 with timer("Loading Hamiltonian"):
-    mat_1, mat_1_zeeman, mat_2, mat_2_minus, mat_2_plus = load_hamiltonian(f"{n}_rubidium87")
-    mat_2_combination = mat_2_plus + mat_2_minus  # Units of a0 e
-    mat_2_combination *= C_e * physical_constants["Bohr radius"][0] / C_hbar
-    # Conversion from atomic units for dipole matrix elements to a Rabi freq in Hz
-    mat_2_combination *= 1e-9  # Convert Hz to GHz
+    mat_1, mat_1_zeeman, mat_2, mat_2_minus, mat_2_plus = load_hamiltonian(f"{n}_rubidium87_relevant")
+    # mat_1, mat_1_zeeman, mat_2, mat_2_minus, mat_2_plus = load_hamiltonian(f"{n}_rubidium87")
 
 with timer("Loading transformations"):
-    transform_1 = load_transformation(n, Basis.N_L_J_MJ, Basis.N_L_ML_MS)
-    transform_2 = load_transformation(n, Basis.N_L_ML_MS, Basis.N1_N2_ML_MS)
+    transform_1 = load_transformation(n, Basis.N_L_J_MJ_RELEVANT, Basis.N_L_ML_MS_RELEVANT)
+    # transform_1 = load_transformation(n, Basis.N_L_J_MJ, Basis.N_L_ML_MS)
 
 with timer("Applying transformation to nlmlms"):
     mat_1 = transform_basis(mat_1, transform_1)
     mat_2 = transform_basis(mat_2, transform_1)
-    mat_2_combination = transform_basis(mat_2_combination, transform_1)
+    mat_2_plus = transform_basis(mat_2_plus, transform_1)
+    mat_2_minus = transform_basis(mat_2_minus, transform_1)
 
-with timer("Applying transformation to n1n2mlms"):
-    mat_1 = transform_basis(mat_1, transform_2)
-    mat_2 = transform_basis(mat_2, transform_2)
-    mat_2_combination = transform_basis(mat_2_combination, transform_2)
-
-with timer("Applying state filters"):
-    indices_to_keep = []
-    for i, (n1, n2, _ml, _ms) in enumerate(states):
-        if _ms > 0 and n1 == 0 and _ml >= 0:
-            # if _ms > 0 and n2 == 0 and _ml >= 0:
-            indices_to_keep.append(i)
-
-    # Sort indices
-    # indices_to_keep = sorted(indices_to_keep, key=lambda i: (states[i][0], states[i][2]))
-    indices_to_keep = sorted(indices_to_keep, key=lambda i: states[i][2])
-
-    mat_1 = mat_1[indices_to_keep, :][:, indices_to_keep]
-    mat_2 = mat_2[indices_to_keep, :][:, indices_to_keep]
-    mat_2_combination = mat_2_combination[indices_to_keep, :][:, indices_to_keep]
-    states = np.array(states)[indices_to_keep]
-
-    states_count = len(states)
-    print(f"Filtered states to {states_count}")
-
-steps = 500
+steps = 50
+steps = 100
 # dc_fields = np.linspace(140, 185, steps)  # V / m
-dc_fields = np.linspace(185, 140, steps)  # V / m
+# dc_fields = np.linspace(185, 140, steps)  # V / m
+# dc_fields = np.linspace(100, 350, steps)  # V / m
+dc_fields = np.linspace(350, 100, steps)  # V / m
+# dc_fields = np.linspace(250, 200, steps)  # V / m
 
-rf_freq = 175e6 / 1e9  # GHz
-e_rf = 1  # V / m
+rf_freq = 230e6 / 1e9  # GHz
+rf_field = 4.6  # V / m
 
 s = 3
 
@@ -75,83 +57,146 @@ energies_with_rf = []
 eigenvectors_with_rf = []
 for i, dc_field in enumerate(tqdm(dc_fields)):
     hamiltonian = mat_1 + dc_field * mat_2
+    eigenvalues, eigenstates, transformation = diagonalise_for_n1n2(states, hamiltonian)
+    hamiltonian_n1n2 = transformation @ hamiltonian @ transformation.T
 
-    eigenvalues = np.diagonal(hamiltonian)
+    _eigenvalues = np.diagonal(hamiltonian_n1n2).copy()
 
-    zero_energy = eigenvalues[s]
-    detunings = np.zeros(states_count)
+    zero_energy = _eigenvalues[s]
+    detunings = np.zeros(len(_eigenvalues))
     for i in range(2 * n - 1):
-        if i >= states_count:
-            break
         if i < n:
-            detunings[i] = (i - s) * rf_freq - (eigenvalues[i] - eigenvalues[s])
+            detunings[i] = (i - s) * rf_freq - (_eigenvalues[i] - _eigenvalues[s])
         else:
-            detunings[i] = rf_freq - (eigenvalues[i - n + 1] - eigenvalues[i]) + detunings[i - n + 1]
+            # detunings[i] = rf_freq - (eigenvalues[i - n + 1] - eigenvalues[i]) + detunings[i - n + 1]
+            detunings[i] = (i - n + 1 - s) * rf_freq - (_eigenvalues[i] - _eigenvalues[s])
 
     detunings *= -1
     energies.append(detunings)
 
     # Construct a new Hamiltonian using the no-RF detunings along the diagonal.
-    hamiltonian_with_rf = e_rf * mat_2_combination + np.diagflat(detunings)
+    mat_2_plus_n1n2 = transformation @ mat_2_plus @ transformation.T
+    mat_2_minus_n1n2 = transformation @ mat_2_minus @ transformation.T
+    hamiltonian_with_rf = rf_field * (mat_2_plus_n1n2 + mat_2_minus_n1n2) / 2 + np.diagflat(detunings)
 
+    # plot_matrices([mat_2_plus_n1n2, transformation @ mat_2_minus @ transformation.T])
+    # plt.show()
     eigenvalues, eigenvectors = np.linalg.eigh(hamiltonian_with_rf)
     energies_with_rf.append(eigenvalues)
     eigenvectors_with_rf.append(eigenvectors)
 
 energies = np.array(energies)
-energies_with_rf = np.array(energies_with_rf)
 
 setup_plot()
-fig, (ax1, ax2) = plt.subplots(
-    1, 2,
-    sharex='all', sharey='all',
-    figsize=(6, 3.5),
-)
+# fig, (ax1, ax2) = plt.subplots(
+#     1, 2,
+#     sharex='all', sharey='all',
+#     figsize=(6, 3.5),
+# )
 
-for i in range(energies.shape[1]):
-    if i == s:
-        color = 'C0'
-        s_ = 3
-        alpha = 0.7
-        zorder = 3
-    elif i == n - 1:
-        color = 'C1'
-        s_ = 3
-        alpha = 0.7
-        zorder = 3
-    else:
-        color = 'grey'
-        s_ = 1
-        alpha = 0.2
-        zorder = 2
-    ax1.scatter(
-        dc_fields / 100,
-        energies[:, i],
-        color=color,
-        alpha=alpha,
-        s=s_,
-        zorder=zorder,
-    )
+if plot_fig17a:
+    for i in range(energies.shape[1]):
+        # Highlighted first and last state for n1 = 0
+        # if i == s:
+        #     color = 'C0'
+        #     s_ = 3
+        #     alpha = 0.7
+        #     zorder = 3
+        # elif i == n - 1:
+        #     color = 'C1'
+        #     s_ = 3
+        #     alpha = 0.7
+        #     zorder = 3
+        # else:
+        #     color = '#222222'
+        #     s_ = 1
+        #     alpha = 0.1
+        #     zorder = 2
 
-eigenvectors_with_rf = np.array(eigenvectors_with_rf)  # [Timestep, eigenvectors, eigenvector]
+        # n1 = 0 only
+        # color = '#1B9E77'
+        # s_ = 3
+        # alpha = 0.7
+        # zorder = 3
+        # if i >= n:
+        #     continue
 
-for i in range(energies_with_rf.shape[1]):
-    _eigenvectors = eigenvectors_with_rf[:, i, :]
-    # colors = []
-    for eigenvector in eigenvectors_with_rf[:, :, i]:
-        eigenvector_component_s = eigenvector[s] ** 2
-        eigenvector_component_max_ml = eigenvector[n - 1] ** 2
-        # colors.append(cmap_with_alpha(eigenvector_component_s, eigenvector_component_max_ml))
+        # n1 = 1 only
+        # color = '#D95F02'
+        # s_ = 3
+        # alpha = 0.7
+        # zorder = 3
+        # if i < n:
+        #     continue
 
-    ax2.scatter(
-        dc_fields / 100,
-        energies_with_rf[:, i],
-        # color='C0',
-        # color=colors,
-        color='grey',
-        s=1,
-        alpha=0.2
-    )
+        if i < n:
+            color = '#1B9E77'
+            s_ = 3
+            alpha = 0.3
+            zorder = 3
+        else:
+            color = '#D95F02'
+            s_ = 3
+            alpha = 0.3
+            zorder = 2
+        # if i < n:
+        #     marker = 'x'
+        #     color = plt.get_cmap('viridis')(i / n)
+        # else:
+        #     continue
+        plt.scatter(
+            dc_fields / 100,
+            energies[:, i],
+            color=color,
+            alpha=alpha,
+            s=s_,
+            zorder=zorder,
+            # marker=marker,
+            edgecolors='none',
+        )
+
+    plt.xlim(dc_fields[0] / 100, dc_fields[-1] / 100)
+    plt.ylim(-3.5, 3.5)
+
+    plt.ylabel('Energy [GHz]')
+    plt.xlabel(r"$E_{\mathrm{d.c.}}$ [V $\mathrm{cm}^{-1}$]")
+    plt.tight_layout(pad=0.5)
+
+    save_current_fig('avoided_crossings_n1_01_coloured_small')
+    plt.show()
+
+
+if plot_fig17b:
+    energies_with_rf = np.array(energies_with_rf)
+    eigenvectors_with_rf = np.array(eigenvectors_with_rf)  # [Timestep, eigenvectors, eigenvector]
+
+    for i in range(energies_with_rf.shape[1]):
+        _eigenvectors = eigenvectors_with_rf[:, i, :]
+        # colors = []
+        for eigenvector in eigenvectors_with_rf[:, :, i]:
+            eigenvector_component_s = eigenvector[s] ** 2
+            eigenvector_component_max_ml = eigenvector[n - 1] ** 2
+            # colors.append(cmap_with_alpha(eigenvector_component_s, eigenvector_component_max_ml))
+
+        plt.scatter(
+            dc_fields / 100,
+            energies_with_rf[:, i],
+            # color='C0',
+            # color=colors,
+            color='grey',
+            s=3,
+            alpha=0.2
+        )
+
+    plt.xlim(dc_fields[0] / 100, dc_fields[-1] / 100)
+    plt.ylim(-3.5, 3.5)
+
+    plt.ylabel('Energy [GHz]')
+    plt.xlabel(r"$E_{\mathrm{d.c.}}$ [V $\mathrm{cm}^{-1}$]")
+    plt.tight_layout(pad=0.5)
+
+    save_current_fig('avoided_crossings_b')
+    plt.show()
 
 #
 # plt.colorbar(
@@ -161,7 +206,8 @@ for i in range(energies_with_rf.shape[1]):
 
 
 plt.xlim(dc_fields[0] / 100, dc_fields[-1] / 100)
-plt.ylim(-1.5, 1.5)
+# plt.ylim(-1.5, 1.5)
+plt.ylim(-3.5, 3.5)
 
 ax1.set_ylabel('Energy [GHz]')
 ax1.set_xlabel(r"$E_{\mathrm{d.c.}}$ [V $\mathrm{cm}^{-1}$]")
